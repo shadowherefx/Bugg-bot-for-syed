cf77ac2047cbe22558923c04f88faf9396475cf4
Shadow <shadowhere667788@gmail.com>
2026-02-21 12:13:02 +0500

    Update config.js

--- config.js
+++ config.js
@@ -1,3 +1,4 @@
-module.exports = {"8398393313:AAH_8LQmtkEIgm_DBaitq_M3HXqxmJ6RT8Q",
+module.exports = {
+  BOT_TOKEN: "8398393313:AAH_8LQmtkEIgm_DBaitq_M3HXqxmJ6RT8Q",
   OWNER_ID: ["8488081516"],
 };
